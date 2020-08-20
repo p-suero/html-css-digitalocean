97298a1dafbc4ce04231ef769236591cd828c037
Paolo <paolo.suero@gmail.com>
2020-08-20 16:46:46 +0200

    Aggiornato popup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 //rimuovo il popup al click del messaggio di chiusura
 document.getElementById("remove-popup").addEventListener("click", function() {
-  document.getElementById("popup-info").remove()
+  document.getElementById("wrapper-popup").remove()
 });
